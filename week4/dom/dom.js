4a7e51bfab2a14a930b10cc8e9e59952d4b386d2
swoyamr.shakya <swoyamr.shakya@patancollege.edu.np>
2024-06-13 10:19:47 +0545

    dom added

---
+++ week4/dom/dom.js
@@ -0,0 +1,5 @@
+// let titleElement = document.getElementById('title');
+// console.log(titleElement.textContent);
+
+let classElement = document.getElementsByClassName('content');
+console.log(classElement);
