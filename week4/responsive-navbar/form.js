b8b4ff458aa3fb7e3b566a33579139a24f7d579a
swoyamr.shakya <swoyamr.shakya@patancollege.edu.np>
2024-06-19 19:37:56 +0545

    css animations

--- week4/responsive-navbar/form.js
+++ week4/responsive-navbar/form.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.getElementById('menuToggle');
     const nav = document.getElementById('navbar');
-    const navItems = document.getElementById('navItems');
+    const hamburger =document.getElementsByClassName('ri-menu-line');
+    const closeLine = document.getElementsByClassName('ri-close-line');
+    const buttonContainer = document.getElementById('navItems');
 
     menuToggle.addEventListener('click', function () {
-        nav.classList.toggle('show');
-        navItems.classList.toggle('show');
-    });
 
-    
+        nav.classList.toggle('show');
+        hamburger.classList.toggle('show');
+        closeLine.classList.toggle('hide');
+        buttonContainer.classList.toggle('hide');
+    });
 });
